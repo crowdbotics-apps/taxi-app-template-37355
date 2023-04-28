5797a7e42c735bb8f2b1dcfcb69aedf7516b3d9f
Evan Shapiro <evan.shapiro@crowdbotics.com>
2023-04-28 10:50:40 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/maps/styles.js
+++ modules/maps/styles.js
@@ -1,27 +1,27 @@
 import { StyleSheet, Dimensions } from "react-native";
+const {
+  width,
+  height
+} = Dimensions.get("window"); // Guideline sizes are based on standard ~5" screen mobile device
 
-const { width, height } = Dimensions.get("window");
-
-// Guideline sizes are based on standard ~5" screen mobile device
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = (size) => (width / guidelineBaseWidth) * size;
-const scaleVertical = (size) => (height / guidelineBaseHeight) * size;
+const scale = size => width / guidelineBaseWidth * size;
+
+const scaleVertical = size => height / guidelineBaseHeight * size;
 
 export const styles = StyleSheet.create({
   view: {
     height: "100%"
   },
-  container: {
-    ...StyleSheet.absoluteFillObject,
+  container: { ...StyleSheet.absoluteFillObject,
     height: 400,
     width: 400,
     justifyContent: "flex-end",
     alignItems: "center"
   },
-  map: {
-    ...StyleSheet.absoluteFillObject
+  map: { ...StyleSheet.absoluteFillObject
   },
   searchbar: {
     backgroundColor: "white",
--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
-  name: "taxi_app_template_37355",
-  url: "https://taxi_app_template_37355.botics.co",
-  api: "https://taxi_app_template_37355.botics.co/api/v1"
-}
+  "name": "taxi_app_template_37355",
+  "url": "https://taxi_app_template_37355.botics.co",
+  "api": "https://taxi_app_template_37355.botics.co/api/v1",
+  "initialRoute": "inviteFriends"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
